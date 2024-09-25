b296f28ab33f670a33a09a20aa0a2e49af3fca78
elina <elina.nep@gmail.com>
2024-09-25 19:54:12 +0200

    fix: terraform fmt backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-rss"
-    key            = "global/s3/terraform.tfstate"
-    region         = "eu-north-1"
-    encrypt        = true
+    bucket  = "terraform-bucket-rss"
+    key     = "global/s3/terraform.tfstate"
+    region  = "eu-north-1"
+    encrypt = true
   }
 }
